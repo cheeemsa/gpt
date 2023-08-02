d61a0ddf832f2f4f73c42d901c69c75b85e2f96c
wangjipeng <1343433174@qq.com>
2023-08-02 17:29:36 +0800

    第一次提交"chatgptapi demo"

---
+++ GPT_API_OWN/demo_add_system_role as sql.py
@@ -0,0 +1,35 @@
+import openai
+
+# 设置您的OpenAI API密钥
+openai.api_key = "sk-2EfkISU4b1fx8AFYgxW9XSR7b4NpjaJA7eAidM2RgiQ6aQsz"
+openai.api_base = "https://api.chatanywhere.cn/v1"
+
+messages = [
+    {"role": "system", "content": "You are a data analyst who is proficient in writing SQL queries."},
+]
+completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages
+    )
+
+    # 从API响应中提取助手的回复
+chat_response = completion.choices[0].message["content"]
+while True:
+    # 用户输入消息
+    content = input("User: ")
+    # 将用户消息添加到消息列表中
+    messages.append({"role": "user", "content": content})
+
+    # 使用OpenAI API进行对话生成
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages
+    )
+
+    # 从API响应中提取助手的回复
+    chat_response = completion.choices[0].message["content"]
+    # 将助手的回复打印出来
+    print(f'ChatGPT: {chat_response}')
+
+    # 将助手的回复添加到消息列表中，准备下一轮对话
+    messages.append({"role": "assistant", "content": chat_response})
---
+++ GPT_API_OWN/demo_count_time.py
@@ -0,0 +1,30 @@
+import openai
+import time
+# 设置您的OpenAI API密钥
+openai.api_key = "sk-2EfkISU4b1fx8AFYgxW9XSR7b4NpjaJA7eAidM2RgiQ6aQsz"
+openai.api_base = "https://api.chatanywhere.cn/v1"
+
+# 定义一个空列表来存储对话消息
+messages = []
+
+while True:
+    # 用户输入消息
+    content = input("User: ")
+    T1 = time.perf_counter()
+    # 将用户消息添加到消息列表中
+    messages.append({"role": "user", "content": content})
+
+    # 使用OpenAI API进行对话生成
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages
+    )
+
+    # 从API响应中提取助手的回复
+    chat_response = completion.choices[0].message["content"]
+    T2 = time.perf_counter()
+    print('程序运行时间:%s毫秒' % ((T2 - T1)*1000))
+    # 将助手的回复打印出来
+    print(f'ChatGPT: {chat_response}')
+    # 将助手的回复添加到消息列表中，准备下一轮对话
+    messages.append({"role": "assistant", "content": chat_response})
